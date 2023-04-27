1844226f2f175c0fbdd4303996f838bbf60bcfa7
GIT13Olena <118273931+GIT13Olena@users.noreply.github.com>
2023-04-27 10:02:29 +0000

    Deploying to gh-pages from @ GIT13Olena/goit-js-hw-09@8d0eeaf069c8e32a42cebf618db95064d81fa485 🚀

---
+++ 01-color-switcher.28b83d7f.js
@@ -0,0 +1,2 @@
+!function(){let t;document.querySelector("[data-start]").addEventListener("click",(()=>{t||(t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),document.querySelector("[data-start]").disabled=!0)})),document.querySelector("[data-stop]").addEventListener("click",(()=>{clearInterval(t),t=null,document.querySelector("[data-start]").disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.28b83d7f.js.map
