cab4cda49959bab9efc660b00405efb34f1c6e21
GIT13Olena <girnyak.1olena@gmail.com>
2023-04-27 14:11:27 +0300

    27.04

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,35 +2,69 @@ import flatpickr from 'flatpickr';
 //style
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startButton = document.querySelector('button[data-start]');
+const timerContainer = document.querySelector('.timer');
+const fields = [...document.getElementsByClassName('field')];
+const timerDays = document.querySelector('span[data-days]');
+const timerHours = document.querySelector('span[data-Hours]');
+const timerMins = document.querySelector('span[data-minutes]');
+const timerSecs = document.querySelector('span[data-seconds]');
+
+let chsnTime = 0;
+
+startButton.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (Date.now() > selectedDates[0].getTime()) {
+      window.alert('Please choose a date in the future!');
+    } else {
+      startButton.disabled = false;
+      chsnTime = selectedDates[0].getTime();
+    }
   },
 };
 
+flatpickr('#datetime-picker', options);
+
+function addLeadingZero(number) {
+  return number.toString().padStart(2, '0');
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+
+  const intervalID = setInterval(() => {
+    let timeLeft = (Date.now() - chsnTime) * -1;
+
+    if (timeLeft <= 0) {
+      clearInterval(intervalID);
+      window.alert('TIME IS OUT');
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    timerDays.textContent = addLeadingZero(days);
+    timerHours.textContent = addLeadingZero(hours);
+    timerMins.textContent = addLeadingZero(minutes);
+    timerSecs.textContent = addLeadingZero(seconds);
+  }, 1000);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-import Notiflix from 'notiflix';
+import Notiflix, { Notify } from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 function createPromise(position, delay) {
@@ -15,20 +15,21 @@ function createPromise(position, delay) {
 }
 
 const form = document.querySelector('.form');
-form.addEventListener('submit', async event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  const delay = +event.target.elements.delay.value;
-  const step = +event.target.elements.step.value;
-  const amount = +event.target.elements.amount.value;
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
 
   for (let i = 1; i <= amount; i++) {
     const position = i;
-    const promiseDelay = delay + step * (i - 1);
-    try {
-      const { position, delay } = await createPromise(position, promiseDelay);
-      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } catch ({ position, delay }) {
-      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
+    const promiseDelay = delay + (i - 1) * step;
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 });
